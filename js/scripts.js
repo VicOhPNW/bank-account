efade8121d85ef4509459a113a1baacdef342efb
Meria Thomas and Victoria Oh <victoria@>
2018-08-21 16:21:31 -0700

    create prototypes

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,15 @@ function BankAccount(deposit, withdraw) {
 }
 
 BankAccount.prototype.balance = function() {
-  return this.initialDeposit + this.deposit + this.withdraw;
+  if(this.deposit){
+    return this.initialDeposit + this.deposit;
+  }else{
+  return this.initialDeposit - this.withdraw;
+}
+}
+
+BankAccount.prototype.addDeposit = function(initialDeposit) {
+  return this.initialDeposit + this.deposit;
 }
 
 
@@ -29,18 +37,19 @@ $(document).ready(function(){
 
     var newBankAccount = new BankAccount(deposit, withdraw);
 
-    $(".amount").append(" $" + newBankAccount.balance());
+    $(".amount").append(" $" + newBankAccount.addDeposit());
 
     var name = $("#name").val("");
     var initialDeposit = parseInt($("#initialDeposit").val(""));
     var deposit = parseInt($("#deposit").val(""));
-    var withdraw = parseInt(-$("#withdraw").val(""));
+    var withdraw = parseInt($("#withdraw").val(""));
     $("ul#userName").append("<li><span class='clickable'>" + newUserInfo.name + "</span></li>");
 
     $(".clickable").last().click(function(){
       $(".showName").show();
       $(".name1").text(newUserInfo.name);
       $(".balance1").text(newUserInfo.initialDeposit);
+      $(".newBalance1").text(newBankAccount.initialDeposit);
     });
 
   });
